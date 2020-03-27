94051c9e4e5d1ea0ea7b70e0b16d6db77d737ab2
Bilal Ahmed Janjua <bilalahmjan@gmail.com>
2020-03-27 22:55:09 +0500

    Css added!

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+
 import App from "./App";
+
+import "antd/dist/antd.css";
 
 const client = new ApolloClient({
   uri: "https://corona-graphql.herokuapp.com/"
